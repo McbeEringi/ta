262a034b259c044355bb4a89fc02aa37a5fa5ca2
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-05-06 20:35:39 +0900

    Update ta.js

--- ta.js
+++ ta.js
@@ -7,7 +7,7 @@ const TA={
 			I=y=>{x.value=V.slice(0,S)+y+V.slice(E);},D=()=>{if(S==E&&(TA.brkt.includes(V.slice(S-1,S+1))||(V[S-1]==V[S]&&TA.brkt.includes(V[S]))))C(S-1,E+1);};
 		(({
 			Tab(){C(S+1,I('	',P()));},Backspace:D,Dete:D,Enter(){const s='\n'+V.slice(0,S).match(/([	 ]*).*$/)[1]+(TA.brkt.some(y=>V[S-1]==y[0])?'	':'');C(S+s.length,I(s,P()));},
-			...Object.fromEntries(TA.brkt.map(y=>y[1]?[[y[0],()=>C(S,I(V.slice(S,E)+y[1]))],[y[1],()=>{if(V[S]==y[1])C(S+1,P());}]]:[[y,()=>{if(V[S]==y)C(S+1,P());if(V[S-1]!=y)C(S,I(V.slice(S,E)+y));}]]).flat())
+			...Object.fromEntries(TA.brkt.flatMap(y=>y[1]?[[y[0],()=>C(S,I(V.slice(S,E)+y[1]))],[y[1],()=>{if(V[S]==y[1])C(S+1,P());}]]:[[y,()=>{if(V[S]==y)C(S+1,P());if(V[S-1]!=y)C(S,I(V.slice(S,E)+y));}]]))
 		})[e.key]||Array)();if(e.defaultPrevented)TA.ah(x);
 	}});}
 };
