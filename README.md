71a8c0d424b507491834618f3b49da1822a917d6
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-09-07 20:41:47 +0900

    -

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # TA.js
+
 A super tiny textarea code editor  
 ![icon](img/icon.svg)
+
 ## features
+
 - Auto height
 - Tab insert
 - Auto bracket
 - Auto indent
-## TA+.js
-Ther is also module version with ranged indent feature.
+- range indent
+
 ## usage
+
 ### methods
+
 - **TA.ah( *HTMLTextAreaElement* ) -> undefined**  
-	updates textarea height according to the content.
+  updates textarea height according to the content.
 - **TA.sizer( *HTMLTextAreaElement* ) -> undefined**  
-	adds TA.ah() to input eventlistner and execute instant.  
-	use this if you only need auto height feature.
+  adds TA.ah() to input eventlistner and execute instant.  
+  use this if you only need auto height feature.
 - **TA.editor( *HTMLTextAreaElement* ) -> undefined**  
-	setup the editor. TA.sizer() also executed.
+  setup the editor. TA.sizer() also executed.
+
 ### properties
+
 - **TA.brkt=['()','{}','[]','\'','"','`']**  
-	defines bracket pairs and quotations.
+  defines bracket pairs and quotations.
+
 ## LICENSE
+
 MIT License. see [LICENSE](LICENSE)
--- ta.js
+++ ta.js
@@ -1,11 +1,13 @@
 self.TA={
 	brkt:['()','{}','[]',...'\'"`'],
-	ah(w){Object.assign(w.style,{boxSizing:'border-box',marginBottom:w.style.height,height:''}).height=w.offsetHeight+w.scrollHeight-w.clientHeight+1+'px';w.style.marginBottom='';},
-	sizer(w){TA.ah(w);w.addEventListener('input',_=>TA.ah(w));},
-	editor(w){TA.sizer(w);w.addEventListener('keydown',(e,{selectionStart:S,selectionEnd:E,value:V}=w,DP,P=_=>e.preventDefault(DP=1),I=(x,y,z=y)=>(x&&(w.value=V.slice(0,S)+x+V.slice(E)),w.selectionStart=y,w.selectionEnd=z),D=_=>S==E&&(TA.brkt.includes(V.slice(S-1,S+1))||(V[S-1]==V[S]&&TA.brkt.includes(V[S])))&&I(0,S-1,E+1))=>e.isComposing||(
-		(({
-			Backspace:D,Delete:D,Tab:_=>I('	',S+1,P()),Enter:(s='\n'+V.slice(0,S).match(/([	 ]*).*$/)[1]+(TA.brkt.some(y=>V[S-1]==y[0])?'	':''))=>I(s,S+s.length,P()),
-			...Object.fromEntries(TA.brkt.flatMap(x=>x[1]?[[x[0],_=>I(V.slice(S,E)+x[1],S)],[x[1],_=>V[S]==x[1]&&I(0,S+1,P())]]:[[x,_=>(V[S]==x&&I(0,S+1,P()),V[S-1]!=x&&I(V.slice(S,E)+x,S))]]))
-		})[e.key]||Array)(),DP&&TA.ah(w)
-	));}
+	ah(w){Object.assign(w.style,{boxSizing:'border-box',transition:'height 0s',marginBottom:w.style.height,height:''}).height=w.offsetHeight+w.scrollHeight-w.clientHeight+1+'px';w.style.marginBottom='';},
+	sizer(w){this.ah(w);w.addEventListener('input',_=>this.ah(w));},
+	editor(w){this.sizer(w);w.addEventListener('keydown',(
+		e,_,{selectionStart:S,selectionEnd:E,value:V}=w,ZS=V.slice(0,S),SE=V.slice(S,E),DP,P=_=>e.preventDefault(DP=1),I=(x,y,z=y,s=S)=>(x!==0&&(w.value=V.slice(0,s)+x+V.slice(E)),w.selectionStart=y,w.selectionEnd=z),
+		D=_=>S==E&&this.brkt.map(x=>x.length<2?x+x:x).includes(V.slice(S-1,E+1))&&I(0,S-1,E+1),C=x=>w.addEventListener('input',_=>I(0,S+1,E+1),{once:1},I(SE+x,S))
+	)=>(e.isComposing||e.metaKey||e.ctrlKey||e.altKey)||(_={
+			Backspace:D,Delete:D,Escape:_=>w.blur(),Tab:(i=0,j=0,LS=ZS.lastIndexOf('\n')+1)=>P()||S==E&&!e.shiftKey?I('	',S+1):I(V.slice(LS,E).replace(/^([	]*)/gm,(_,x,k)=>(e.shiftKey?x?x.slice(0,-1,k||(i--),j--):x:(k||(i++),j++,x+'	'))),S+i,E+j,LS),
+			Enter:(s='\n'+ZS.match(/([	 ]*).*$/)[1]+(this.brkt.some(x=>V[S-1]==x[0])?'	':''))=>I(s,S+s.length,P()),...Object.fromEntries(this.brkt.flatMap(x=>x[1]?[[x[0],_=>C(x[1])],[x[1],_=>V[S]==x[1]&&I(0,S+1,P())]]:[[x,[_=>C(x),,_=>I(0,S+1,P())]['0212'[((V[S]==x)+(V[S-1]==x)*2)*(S==E)]]]]))
+		}[e.key],_&&_(),DP&&this.ah(w))
+	);}
 };
